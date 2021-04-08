7a764e238c664badc8bc3ff3caf1f84d94df3dc2
Lucas Bazemore <lucasabazemore@gmail.com>
2021-04-08 17:25:14 -0600

    fix typescript error

--- app/choices/mutations/createChoice.ts
+++ app/choices/mutations/createChoice.ts
@@ -1,17 +1,17 @@
 import { resolver } from 'blitz';
-import db from 'db';
+import db, { Choice } from 'db';
 import * as z from 'zod';
 
 const CreateChoice = z
   .object({
-    name: z.string()
+    text: z.string()
   })
   .nonstrict();
 
 export default resolver.pipe(
   resolver.zod(CreateChoice),
   resolver.authorize(),
-  async (input) => {
+  async (input: Choice) => {
     // TODO: in multi-tenant app, you must add validation to ensure correct tenant
     const choice = await db.choice.create({ data: input });
 
